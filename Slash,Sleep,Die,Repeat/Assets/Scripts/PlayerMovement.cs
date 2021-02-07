cc4613842dfb0afa31d1870c98b8ced109d9962f
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-02-08 09:50:55 +1100

    Setup base scene
    Added player movement, and made a basic scene with the brackeys 2d mega pack. Also added a enemy in that animates.

---
+++ Slash,Sleep,Die,Repeat/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    SpriteRenderer playerRenderer;
+    [SerializeField] List<Sprite> playerSprites;
+    Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRenderer.sprite = playerSprites[0];
+            rb.AddForce(new Vector2(0, 1 * speed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRenderer.sprite = playerSprites[1];
+            rb.AddForce(new Vector2(-1 * speed, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRenderer.sprite = playerSprites[2];
+            rb.AddForce(new Vector2(0, -1 * speed));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRenderer.sprite = playerSprites[3];
+            rb.AddForce(new Vector2(1 * speed, 0));
+        }
+    }
+}

23091cbbd9b80aceb706fc0b971277a37376d7ce
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-03-01 12:38:33 +1100

    Built map
    Built map, imported 2d tilemap package. Started playing around with ai pathfinding

---
+++ Slash,Sleep,Die,Repeat/Assets/Scripts/Pathfinding/Grid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+	public Transform startPos;
+	public LayerMask obstacleMask;
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	public float distance;
+
+	Node[,] grid;
+	public List<Node> FinalPath;
+	float nodeDiameter;
+	int gridSizeX, gridSizeY;
+
+	private void Start()
+	{
+		nodeDiameter = nodeRadius * 2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+		CreateGrid();
+	}
+
+	void CreateGrid()
+	{
+		grid = new Node[gridSizeX, gridSizeY];
+		//Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3
+	}
+}
---
+++ Slash,Sleep,Die,Repeat/Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+	public int x;
+	public int y;
+
+	public bool isObstacle;
+	public Vector3 pos;
+	public Node parent;
+
+	public int gCost;
+	public int hCost;
+	public int FCost { get { return gCost + hCost; } }
+
+	public Node(bool obstacle, Vector3 p, int _x, int _y)
+	{
+		isObstacle = obstacle;
+		pos = p;
+		x = _x;
+		y = _y;
+	}
+
+}

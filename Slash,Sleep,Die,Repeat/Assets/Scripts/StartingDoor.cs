211d034bc0e5f6f2b6b047e7b67a4c36e295b07c
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-03-03 12:03:10 +1100

    Finalised map
    Made map changes to allow the player to fully explore the map. The starting door in now a wall that fades when the player get a full build. Imported the 2d sprite pack so we can edit tiles inside a tilemap.

--- Slash,Sleep,Die,Repeat/Assets/Scripts/StartingDoor.cs
+++ Slash,Sleep,Die,Repeat/Assets/Scripts/StartingDoor.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class StartingDoor : MonoBehaviour
 {
 	[SerializeField] BoxCollider2D collisionBox;
-	private void OnTriggerEnter2D(Collider2D collision)
+	List<Animator> animations;
+
+	private void Start()
+	{
+		animations = new List<Animator>(GetComponentsInChildren<Animator>());
+	}
+	private void OnTriggerStay2D(Collider2D collision)
 	{
 		if(Player.instance.GetStats().ApplyBuild())
 		{
 			collisionBox.isTrigger = true;
+			foreach(var ani in animations)
+			{
+				ani.SetBool("Activate", true);
+			}
+
 		}
 		else //make sure the player isnt leaving the starting room without a full build
 		{
